1f104b72d173b590f8e9e8c667a681118d485756
Jeffrey777A <44594550+Jeffrey777A@users.noreply.github.com>
2020-12-03 00:53:04 +0000

    added mobile nav

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-// Smooth Scrolling
-$('.cf a').on('click', function(event) {
-  if (this.hash !== '') {
-    event.preventDefault();
-
-    const hash = this.hash;
-
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top
-      },
-      200,
-      function() {
-        window.location.hash = hash;
-      }
-    );
-  }
-});
+(function($){
+  $(function(){
+    $('.sidenav').sidenav();
+  });
+})(jQuery);
